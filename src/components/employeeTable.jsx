37df68092a1a496cc2d653470a6cbf3e02bb82b4
carlastjuste <carlastjuste@users.noreply.github.com>
2021-03-23 01:23:35 -0400

    update employee table

--- src/components/employeeTable.jsx
+++ src/components/employeeTable.jsx
@@ -12,23 +12,29 @@ async componentDidMount () {
         const res = await API.getUsers(); 
         const employeeList = res.data.results;
         this.setState({employeeList : employeeList});
+        console.log(employeeList);
 }
 
     render() { 
+        let data = this.state.employeeList;
+        console.log(data);
+        //let image = data.picture.thumbnail;
+        //let name = data.name.first + ' ' + data.name.last;
 
         
 
         return (
             <div>
                 <table class="table">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
+                    <thead class="thead-dark">
+                        <tr>
+                        <th scope="col">Image</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">DOB</th>
+                        </tr>
+                    </thead>
   <tbody>
     <tr>
       <th scope="row">1</th>

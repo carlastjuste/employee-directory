d74336bb45836ced6a264763a0cbd85e0b573a41
carlastjuste <carlastjuste@users.noreply.github.com>
2021-03-23 20:00:00 -0400

    Add search area

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import React from 'react';
 import Header from './components/header';
 import EmployeeTable from './components/employeeTable';
+import SearchArea from './components/searchArea'
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <SearchArea />
       <EmployeeTable />
     </React.Fragment>
   );
--- src/components/searchArea.jsx
+++ src/components/searchArea.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import EmployeeTable from "./employeeTable";
 
 class SearchArea extends Component {
     state = {  }
     render() { 
-        return (  );
+        return ( 
+            <div className = "search-area">
+                <input className = "form-control" 
+                type="search" 
+                placeholder="Search...." 
+                />
+            </div>
+         );
     }
 }
  
